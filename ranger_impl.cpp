d4b87e52c3d005a8c6fa0a37f98d2dcedf800c1d
Carl Edquist <edquist@cs.wisc.edu>
2019-10-29 15:56:33 -0500

    make auto types explicit
    ... since they're not hard to type anymore

--- ranger_impl.cpp
+++ ranger_impl.cpp
@@ -9,46 +9,46 @@ ranger::iterator ranger::insert(ranger::range r)
         return forest.insert(r).first;
 
     // NOTE: use upper_bound for fracturing (ie, not to coalesce)
-    auto it_start = forest.lower_bound(r.start);
+    iterator it_start = forest.lower_bound(r.start);
     if (it_start == forest.end())
         return forest.insert(r).first;
 
-    auto it = it_start;
+    iterator it = it_start;
     while (it != forest.end() && it->start <= r.end)  // '<' for fracturing
         ++it;
 
-    auto it_end = it;
+    iterator it_end = it;
     if (it_start == it_end)
         return forest.insert(it_end, r);
 
-    auto it_back = --it;
+    iterator it_back = --it;
     range ir_new = { std::min(it_start->start, r.start),
                      std::max(it_back->end, r.end) };
 
-    auto hint = forest.erase(it_start, it_end);
+    iterator hint = forest.erase(it_start, it_end);
 
     return forest.insert(hint, ir_new);
 }
 
 ranger::iterator ranger::erase(ranger::range r)
 {
-    auto it_start = forest.upper_bound(r.start);
+    iterator it_start = forest.upper_bound(r.start);
     if (it_start == forest.end())
         return it_start;
 
-    auto it = it_start;
+    iterator it = it_start;
     while (it != forest.end() && it->start < r.end)
         ++it;
 
-    auto it_end = it;
+    iterator it_end = it;
     if (it_start == it_end)
         return it_start;
 
-    auto it_back = --it;
+    iterator it_back = --it;
     range ir_start = *it_start;
     range ir_back  = *it_back;
 
-    auto hint = forest.erase(it_start, it_end);
+    iterator hint = forest.erase(it_start, it_end);
     if (ir_start.start < r.start)
         hint = forest.insert(hint, {ir_start.start, r.start});
 
@@ -61,7 +61,7 @@ ranger::iterator ranger::erase(ranger::range r)
 std::pair<ranger::iterator, bool>
 ranger::find(int_type x) const
 {
-    auto it = forest.upper_bound(x);
+    iterator it = forest.upper_bound(x);
     return {it, it != forest.end() && it->start <= x};
 }
 
@@ -75,7 +75,7 @@ std::ostream &operator<<(std::ostream &os, const ranger::range &ir)
 std::ostream &operator<<(std::ostream &os, const ranger &r)
 {
     os << "{";
-    for (auto ir : r.forest)
+    for (ranger::range ir : r.forest)
         os << " " << ir;
     return os << " }";
 }
