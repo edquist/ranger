8b006b29410faf1eaf64ab6731e926945935d11e
Carl Edquist <edquist@cs.wisc.edu>
2019-10-29 18:31:19 -0500

    whitespace

--- ranger.h
+++ ranger.h
@@ -5,9 +5,9 @@
 
 struct ranger {
     struct range;
-    typedef int                       int_type;
-    typedef std::set<range>           set_type;
-    typedef set_type::iterator        iterator;
+    typedef int                 int_type;
+    typedef std::set<range>     set_type;
+    typedef set_type::iterator  iterator;
 
     iterator insert(range r);
     iterator erase(range r);
